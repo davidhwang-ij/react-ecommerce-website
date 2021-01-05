20e1a9c927707d36aa448a495661f440650079d5
David Hwang <davidhwang.ij@gmail.com>
2021-01-04 22:53:52 -0500

    Added order screen

--- backend/controller/orderController.js
+++ backend/controller/orderController.js
@@ -4,7 +4,7 @@ import Order from '../models/orderModel.js'
 // @desc Create new order
 // @route POST /api/orders
 // @access Private
-const addOrderItems = asyncHandler(async (req, res) => {
+export const addOrderItems = asyncHandler(async (req, res) => {
   const {
     orderItems,
     shippingAddress,
