f94b60993522055a51356a168267d88cba1efe04
David Hwang <davidhwang.ij@gmail.com>
2021-01-28 15:28:04 -0500

    Added an order screen, reducer, and action

--- backend/controller/orderController.js
+++ backend/controller/orderController.js
@@ -4,7 +4,7 @@ import Order from '../models/orderModel.js'
 // @desc Create new order
 // @route POST /api/orders
 // @access Private
-export const addOrderItems = asyncHandler(async (req, res) => {
+const addOrderItems = asyncHandler(async (req, res) => {
   const {
     orderItems,
     shippingAddress,
@@ -34,3 +34,22 @@ export const addOrderItems = asyncHandler(async (req, res) => {
     res.status(201).json(createdOrder)
   }
 })
+
+// @desc Get order by ID
+// @route GET /api/orders/:id
+// @access Private
+const getOrderById = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id).populate(
+    'user',
+    'name email'
+  )
+
+  if (order) {
+    res.json(order)
+  } else {
+    res.status(404)
+    throw new Error('Order not found')
+  }
+})
+
+export { addOrderItems, getOrderById }
